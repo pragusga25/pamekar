32b8d7c9080bd1dcf42782983bb22e2199bd53cc
Taufik Pragusga <pragusga25@gmail.com>
2021-08-19 23:02:25 +0700

    loop through projectlist

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectList = [
+{'id':1, 'title': 'Compfest13', 'description': 'Awesome WebApp'},
+{'id':2, 'title': 'BETIS2020', 'description': 'Responsive web'},
+{'id':3, 'title': 'Mentoring', 'description': 'Mentoring Website'},
+]
+
 # Create your views here.
 def projects(request):
     msg = 'Hello World'
     age = 30
-    context = {'msg': msg, 'age': age}
+    context = {'msg': msg, 'age': age, 'projectList':projectList}
     return render(request, 'projects/projects.html', context)
 
 def project(request):
