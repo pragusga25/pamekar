88373cd8b26b5a2f2979c2ad8d013cf11cb9b73d
Taufik Pragusga <pragusga25@gmail.com>
2021-08-20 09:26:06 +0700

    crud

--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.projects, name="projetcs"),
-    path("project/<str:pk>", views.project, name="project"),
-    path("create-project", views.createProject, name="create-project"),
+    path("", views.projects, name="projects"),
+    path("project/<str:pk>/", views.project, name="project"),
+    path("create-project/", views.createProject, name="create-project"),
+    path(
+        "update-project/<str:pk>/", views.updateProject, name="update-project"
+    ),
+    path(
+        "delete-project/<str:pk>/", views.deleteProject, name="delete-project"
+    ),
 ]
