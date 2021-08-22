4078e98439ce6faed933e4b1574b1632df6979d5
Taufik Pragusga <pragusga25@gmail.com>
2021-08-22 15:01:26 +0700

    static files

--- projects/models.py
+++ projects/models.py
@@ -6,6 +6,9 @@ class Project(models.Model):
     id = models.UUIDField(
         default=uuid.uuid4, unique=True, primary_key=True, editable=False
     )
+    featured_image = models.ImageField(
+        null=True, blank=True, default="default.png"
+    )
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
