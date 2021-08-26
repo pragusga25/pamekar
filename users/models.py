a4c207230aa1be5a349773169c4b5bcaf986537e
Taufik Pragusga <pragusga25@gmail.com>
2021-08-26 20:32:54 +0700

    create skill model

--- users/models.py
+++ users/models.py
@@ -9,6 +9,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True, blank=True)
     username = models.CharField(max_length=200, null=True, blank=True)
+    location = models.CharField(max_length=300, null=True, blank=True)
     email = models.CharField(max_length=200, null=True, blank=True)
     short_intro = models.CharField(max_length=200, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
@@ -25,3 +26,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+
+class Skill(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, null=True, blank=True
+    )
+    name = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
