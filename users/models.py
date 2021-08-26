9430f7b7fb6c2a8161a600b934ae8f0a0d1a67f5
Taufik Pragusga <pragusga25@gmail.com>
2021-08-26 19:47:45 +0700

    create profiles model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+class Profile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    short_intro = models.CharField(max_length=200, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    profile_image = models.ImageField(
+        upload_to="profiles/",
+        blank=True,
+        null=True,
+        default="profiles/default.jpg",
+    )
+    social_github = models.CharField(max_length=200, null=True, blank=True)
+    social_twitter = models.CharField(max_length=200, null=True, blank=True)
+    social_linkedin = models.CharField(max_length=200, null=True, blank=True)
+    social_youtube = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user.username)
