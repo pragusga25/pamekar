aae9a5ab1c801f46edf6977a7ce48943dee27217
Taufik Pragusga <pragusga25@gmail.com>
2021-08-26 21:42:54 +0700

    finished adding and rendering profiles

--- users/views.py
+++ users/views.py
@@ -8,3 +8,16 @@ def profiles(request):
     profiles = Profile.objects.all()
     context = {"profiles": profiles}
     return render(request, "users/profiles.html", context)
+
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+
+    context = {
+        "profile": profile,
+        "topSkills": topSkills,
+        "otherSkills": otherSkills,
+    }
+    return render(request, "users/user-profile.html", context)
