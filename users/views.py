fbb2c9e3088fc2bd184ff3ec0913cffc396e84d2
Taufik Pragusga <pragusga25@gmail.com>
2021-09-02 19:17:25 +0700

    prevent authenticated user to login page

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from .models import Profile
 from django.contrib.auth.models import User
 
@@ -25,7 +25,10 @@ def userProfile(request, pk):
     return render(request, "users/user-profile.html", context)
 
 
-def loginPage(request):
+def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect("profiles")
+
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -44,3 +47,8 @@ def loginPage(request):
             print("Username or password is incorrect")
 
     return render(request, "users/login-register.html")
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect("login")
