c76b06f06e548d7a58d2b7879b6db2ad15ea2a4a
Taufik Pragusga <pragusga25@gmail.com>
2021-09-02 17:51:05 +0700

    create signals file

--- users/models.py
+++ users/models.py
@@ -1,8 +1,7 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
+
 
 # Create your models here.
 class Profile(models.Model):
@@ -41,25 +40,3 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
-
-
-# @receiver(post_save, sender=Profile)
-def createProfile(sender, instance, created, **kwargs):
-    if created:
-        user = instance
-        profile = Profile.objects.create(
-            user=user,
-            username=user.username,
-            email=user.email,
-            name=user.first_name,
-        )
-
-
-# @receiver(post_delete, sender=Profile)
-def deleteUser(sender, instance, **kwargs):
-    user = instance.user
-    user.delete()
-
-
-post_save.connect(createProfile, sender=User)
-post_delete.connect(deleteUser, sender=Profile)
